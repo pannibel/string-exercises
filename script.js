edd8bc74323910f5a6c586630012cb74cc528d87
Panni Belevski <pannibel@gmail.com>
2022-08-29 22:19:20 +0200

    if functions in progress

--- script.js
+++ script.js
@@ -10,17 +10,18 @@ let inputbox = document.querySelector("#input");
 
 //EVENT LISTENERS
 button.addEventListener("click", readInput);
-button.addEventListener("click", checkInput);
-button.addEventListener("click", readOption);
-button.addEventListener("click", printOutput);
-button.addEventListener("click", makeOutput);
+//button.addEventListener("click", checkInput);
+//button.addEventListener("click", readOption);
+//button.addEventListener("click", makeOutput);
+//button.addEventListener("click", printOutput);
 inputbox.addEventListener("click", clearInput);
 
 
 //FUNCTIONS
 function readInput() {
     input = document.querySelector("#input").value;
-    console.log(input)
+    console.log(input);
+    checkInput();
 }
 
 //checking if the input is a first name, a full name, a filename, a password or else
@@ -28,8 +29,7 @@ function checkInput() {
 
     let firstLetter = input[0];
 
-
-    if (!input.includes(" ") && firstLetter.toUpperCase() === input[0] && !input.endsWith(".jpg") && !input.endsWith(".png")) {
+    if (!input.includes(" ") && !input.endsWith(".jpg") && !input.endsWith(".png")) {
         inputtype = "firstname"
     };
     if (input.includes(" ") && firstLetter.toUpperCase() === input[0] && !input.endsWith(".jpg") && !input.endsWith(".png")) {
@@ -38,45 +38,64 @@ function checkInput() {
     if (!input.includes(" ") && input.endsWith(".jpg") || !input.includes(" ") && input.endsWith(".png")) {
         inputtype = "file"
     };
-    //how to check if it includes numbers and letters
+    //how to check if it includes numbers and letters? or what makes it a password?
     if (input.includes()) {
 
     };
 
-    console.log(inputtype)
+    console.log(inputtype);
+    readOption();
 }
 
 function readOption() {
     option = document.querySelector("#options").value;
     console.log(option);
+
+    makeOutput();
 }
 
 function makeOutput() {
 
-    if (option === 1) {
-        
+    if (option === "1" && inputtype === "firstname") {
+        //Make the first character in a name uppercase, and the rest lowercase
+        let uppercase = input.substring(0, 1).toUpperCase();
+        let lowercase = input.substring(1).toLowerCase();
+        output = uppercase+lowercase;
     };
-    if (option === 2) {
+    if (option === "2" && inputtype === "fullname") {
+        //Find the first name
+        let firstname = input.substring(input[0], input.indexOf(" "));
+        output = firstname;
+    };
+    if (option === "3" && inputtype === "fullname") {
+        //Find the length of the first name
+        let firstname = input.substring(input[0], input.indexOf(" "));
+        output = firstname.length;
+    };
+    if (option === "4" && inputtype === "fullname") {
+        //Find the middle name start and end position, and the middle name itself in a full name string
 
     };
-    if (option === 3) {
+    if (option === "5" && inputtype === "filename") {
+        //Check if filename is .jpg or .png
+    };
+    if (option === "6" && inputtype === "password") {
+        //Hide a password with the correct number of *s
 
     };
-    if (option === 4) {
+    if (option === "7") {
+        //Make the third character uppercase
+        let uppercase = input.substring(2, 3).toUpperCase();
+        let lowercase1 = input.substring(0, 2).toLowerCase();
+        let lowercase2 = input.substring(3).toLowerCase();
+        output = lowercase1+uppercase+lowercase2;
+    };
+    if (option === "8") {
+        //Make a character uppercase, if it follows a space or a hyphen
 
     };
-    if (option === 5) {
 
-    };
-    if (option === 6) {
-
-    };
-    if (option === 7) {
-
-    };
-    if (option === 8) {
-
-    };
+    printOutput();
 }
 
 function printOutput() {
